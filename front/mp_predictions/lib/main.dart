9cb9b544cf4b30b74b0e018e3d78b17e7d40ae4c
Vitor <vitor.oliveira@altbank.co>
2022-03-28 13:36:19 -0300

    add flutter

---
+++ front/mp_predictions/lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:mp_predictions/src/prediction/prediction.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+final GlobalKey<NavigatorState> navigatorKey = new GlobalKey<NavigatorState>();
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: MaterialApp(
+        title: 'eArbo',
+        localizationsDelegates: const [
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        debugShowCheckedModeBanner: false,
+        supportedLocales: const [
+          Locale('pt', 'BR'), // English
+        ],
+        initialRoute: "/",
+        navigatorKey: navigatorKey,
+        routes: <String, WidgetBuilder>{
+          "/": (BuildContext context) => const Predicition(),
+          // "/about": (BuildContext context) => new RecuperarSenha(),
+          // "/references": (BuildContext context) => new Home(),
+        },
+      ),
+    );
+  }
+}
+
